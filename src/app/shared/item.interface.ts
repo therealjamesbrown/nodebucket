39135ca167801647845df410f016e1e7f98dd68b
James Brown <james@Jamess-iMac.local>
2020-10-07 21:20:31 -0500

    updating a lot

--- src/app/shared/item.interface.ts
+++ src/app/shared/item.interface.ts
@@ -12,16 +12,6 @@
 
 import { ObjectUnsubscribedError } from 'rxjs';
 
-/**
- * 
- * ================================
- * ; Title: Nodebucket Project
- * ; Author: James Brown/Professor Krasso
- * ; Modified by: James Brown
- * ; Date: 9/25/2020
- * ; Description: Nodebucket project for web-450
- * 
- */
 
 export interface Item {
     _id: string;
